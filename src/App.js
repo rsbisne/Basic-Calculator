7c76a09a4c93d7837e2ca3d8e0c8928b8e493d1f
rsbisne <roshansb99@gmail.com>
2023-07-23 16:01:41 +0530

    created basic calculator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import Keypad from "./Components/Keypad";
+import "./index.css";
 function App() {
-  return <div>calculator</div>;
+  const [input, setInput] = useState("");
+  const handleClick = (value) => {
+    setInput(input + value);
+  };
+
+  const handleEqual = () => {
+    // eslint-disable-next-line no-eval
+    setInput(eval(input).toString());
+  };
+
+  const handleClear = () => {
+    setInput("");
+  };
+  return (
+    <div className="App">
+      <div className="calculator">
+        <input className="display" type="text" value={input} readOnly />
+        <Keypad
+          handleClear={handleClear}
+          handleClick={handleClick}
+          handleEqual={handleEqual}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
